64640dc53ec2c24388ec5aa46c6ee2a40800f960
allisson012 <allissoncastilho@gmail.com>
2025-07-15 14:03:54 -0300

    Criando o UserRepository

---
+++ src/main/java/com/allissonsilva/todosimple/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.allissonsilva.todosimple.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.allissonsilva.todosimple.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
